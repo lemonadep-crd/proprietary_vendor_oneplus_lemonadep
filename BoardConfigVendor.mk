a4a9e5d344014717f43b485d3221a11c515f21ee
Lup Gabriel <gwolf2u.website@gmail.com>
2025-01-07 11:56:09 +0200

    Revert "lemonadep: Add firmware"
    This reverts commit 89ac37906d165d0317b5f5eb10b4f0fe8abbb9fd.

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,27 +2,3 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    bluetooth \
-    cpucp \
-    devcfg \
-    dsp \
-    engineering_cdt \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    multiimgoem \
-    oplus_sec \
-    oplusstanvbk \
-    qupfw \
-    qweslicstore \
-    shrm \
-    splash \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
